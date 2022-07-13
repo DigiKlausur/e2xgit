41ad2de73703b1464dd4e124f80c048c3587bb29
Tim Metzler <tim.metzler@h-brs.de>
2022-07-13 15:36:04 +0200

    Add __all__ to __init__.py

--- e2xgit/__init__.py
+++ e2xgit/__init__.py
@@ -1,3 +1,4 @@
 from .e2xrepo import E2xRepo
-from .hooks import configure_post_save_hook
-from .utils import get_author
+from .hooks import configure_e2xgit
+
+__all__ = ["E2xRepo", "configure_e2xgit"]
